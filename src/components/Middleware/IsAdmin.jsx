b925e9f289972461f17d90f70487b0513a495954
Chipskein <abfn0905@gmail.com>
2022-12-08 06:04:45 -0300

    fix:middleware

--- src/components/Middleware/IsAdmin.jsx
+++ src/components/Middleware/IsAdmin.jsx
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 
 export default function IsAdmin ({ children }) {
-    const user = localStorage.getItem('user');
+    let user = localStorage.getItem('user');
+    user=JSON.parse(user)
     const navigate = useNavigate()
-
     useEffect(() => {
-        if(user && !user.is_data_master){
+        if(!user.is_data_master){
             navigate("/jogos")
         }
     }, [])
-    
-
     return children
 }
